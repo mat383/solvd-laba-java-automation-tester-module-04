dca1ca74d13e8d4fe3f8a6a6ac0c15f78543bc5d
Your Name <you@example.com>
2024-02-09 16:16:15 +0100

    fixed missing closing bracket and json schema

--- src/main/java/com/solvd/laba/testing/api/api/GetAllPosts.java
+++ src/main/java/com/solvd/laba/testing/api/api/GetAllPosts.java
@@ -5,7 +5,7 @@ import com.zebrunner.carina.api.annotation.Endpoint;
 import com.zebrunner.carina.api.apitools.builder.NotStringValuesProcessor;
 import com.zebrunner.carina.api.http.HttpMethodType;
 
-@Endpoint(url = "${config.api_url/posts/1", methodType = HttpMethodType.GET)
+@Endpoint(url = "${config.api_url}/posts", methodType = HttpMethodType.GET)
 public class GetAllPosts extends AbstractApiMethodV2 {
     public GetAllPosts() {
         ignorePropertiesProcessor(NotStringValuesProcessor.class);
